e69939a81883994c45c80a2e97a4a4192bd463c7
eneseren-tr <eneseren.off@gmail.com>
2020-07-19 20:54:38 +0300

    Veritabanı

--- Sirigago/settings.py
+++ Sirigago/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'hotel.apps.HotelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
