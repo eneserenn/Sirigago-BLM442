a4ea6f894cfefc7c220fda78536e092df5b4a68a
eneserenn <dev.eneseren@gmail.com>
2020-08-01 05:56:27 +0300

    6-7-8. videolar
    acaba fotografları yüklerken de gösterebilir miyiz?

--- hotel/admin.py
+++ hotel/admin.py
@@ -3,6 +3,12 @@ from django.contrib import admin
 # Register your models here.
 from hotel.models import Category
 from hotel.models import Product
+from hotel.models import Images
+
+class ImagesInline(admin.TabularInline):
+    model = Images
+    extra = 3
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title' , 'status']
@@ -11,7 +17,14 @@ class CategoryAdmin(admin.ModelAdmin):
 admin.site.register(Category,CategoryAdmin)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title' , 'status','category', 'price']
+    list_display = ['title' , 'status','category', 'price','image_tag']
     list_filter = ['price','star']
+    inlines = [ImagesInline]
 
 admin.site.register(Product,ProductAdmin)
+
+class ImagesAdmin(admin.ModelAdmin):
+    list_display = ['title','hotel','image_tag']
+   
+
+admin.site.register(Images,ImagesAdmin)
